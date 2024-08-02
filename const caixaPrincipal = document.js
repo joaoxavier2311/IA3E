686d101db5ac17f35eaf64866629be13c0c58e31
joaoxavier2311 <110679352+joaoxavier2311@users.noreply.github.com>
2024-08-02 09:27:56 -0300

    Add files via upload

---
+++ const caixaPrincipal = document.js
@@ -0,0 +1,11 @@
+const caixaPrincipal = document.querySelector('caixa-principal');
+const caixaPrincipal = document.querySelector('caixa-pergunta');
+const caixaPrincipal = document.querySelector('caixa-alternativa');
+const caixaPrincipal = document.querySelector('caixa-resultado');
+
+const perguntas = [//abre a lista de objetos (itens)
+    (//abrem o item
+        enunciado:"pergunta 1",
+    "alternativa 1",
+    "alternativa 2")
+]
